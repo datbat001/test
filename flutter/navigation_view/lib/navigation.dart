0cd989bcf62bd28854e144cd78f9c209e402d36c
datbat001 <76512448+datbat001@users.noreply.github.com>
2022-12-18 02:51:58 -0600

    creacion de proyeecto en flutter para practicas

--- flutter/navigation_view/lib/navigation.dart
+++ flutter/navigation_view/lib/navigation.dart
@@ -36,8 +36,10 @@ class MyHomePage extends StatelessWidget {
         child: ListView(
           children: [
             UserAccountsDrawerHeader(
-              accountName: Text("Datbat Valerio", style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))),
-              accountEmail: Text("davidalejandro1240@gmail.com", style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))),
+              accountName: Text("Datbat Valerio",
+                  style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))),
+              accountEmail: Text("davidalejandro1240@gmail.com",
+                  style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))),
               currentAccountPicture: CircleAvatar(
                 backgroundImage: NetworkImage(
                     "https://scontent.fcvj2-1.fna.fbcdn.net/v/t39.30808-6/281797413_7271840936221835_1265146639117764745_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGs-1YTKj34RaSVx6pZuxYVZ8bTxOZ5pttnxtPE5nmm20ud26DLMHJ4bEO6VlSqDajOY3fvVjPNeC0AT7Ee-hT2&_nc_ohc=3KyJJ7CIHFUAX8cGaOv&_nc_ht=scontent.fcvj2-1.fna&oh=00_AfCuZ_vA9E0VoUdAGh0Zq0MBMKYrkNbv8dCQi4SULeEyDw&oe=63A11985"),
